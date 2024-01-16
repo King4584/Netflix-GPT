8083bb89ef22028fe4922c6bf776433e981f6301
Utsav <pk0385599@gmail.com>
2024-01-16 19:39:25 +0530

    Added Sign In and Sign Up feature in LogIn Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <header className=''>
-        <img className='m-4 p-2 w-40 relative' src='https://imgs.search.brave.com/ZBbMMUhTlxq8zjlbHNC86k8dpzfNuKb9-zoA7QKevbE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8w/LzBjL05ldGZsaXhf/MjAxNF9sb2dvLnN2/Zw.svg' alt=' netflix-logo'></img>
-      </header>
+    <div className="absolute w-full px-8 py-6 z-10 bg-gradient-to-b from-black">
+        <img
+          className="w-44"
+          src="https://imgs.search.brave.com/ZBbMMUhTlxq8zjlbHNC86k8dpzfNuKb9-zoA7QKevbE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8w/LzBjL05ldGZsaXhf/MjAxNF9sb2dvLnN2/Zw.svg"
+          alt=" netflix-logo"
+        />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Header from './Header'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [isLogedIn , setIsLogedIn] = useState(false);
+  const userSignUp = ()=>{
+    setIsLogedIn(!isLogedIn);
+  }
+
   return (
     <div>
         <Header/>
-        Login
+        <div className='absolute' >
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='background-img'/>
+        </div>
+        <form className='absolute w-3/12 my-36 mx-auto right-0 left-0 bg-opacity-75  bg-black text-white'>
+          <div className='mt-8 mx-8 mb-2  text-3xl'>{(!isLogedIn) ? "Sign In" : "Sign Up" }</div>
+          <input type='email' placeholder='Email or Phone number' className='p-2 mx-8 my-2 w-8/12 bg-gray-600' />
+          <input type='password' placeholder='Password' className='p-2 mx-8 my-2 w-8/12 bg-gray-600' />
+          <button className='p-2 mx-8 my-2 bg-red-600 w-8/12'>Sign In </button>
+          <h3 className='m-2 p-2' onClick={userSignUp}> {(!isLogedIn) ? "New to Netflix? Sign Up Now" : "Already a User ! Sign In Now" } </h3>
+          <h6 className='m-2 p-2 w-10/12 '>This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.</h6>
+        </form>
     </div>
   )
 }
