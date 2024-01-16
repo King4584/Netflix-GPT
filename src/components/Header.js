accb4ff36572244829aaecba7ffb703945f92dab
Utsav <pk0385599@gmail.com>
2024-01-16 18:27:50 +0530

    Config Tailwind

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className=''>
+        <img src='https://imgs.search.brave.com/ZBbMMUhTlxq8zjlbHNC86k8dpzfNuKb9-zoA7QKevbE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8w/LzBjL05ldGZsaXhf/MjAxNF9sb2dvLnN2/Zw.svg' alt=' netflix-logo'></img>
+      </header>
+    </div>
+  )
+}
+
+export default Header
